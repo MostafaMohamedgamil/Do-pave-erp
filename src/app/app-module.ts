515d365b02848e02fce69ee67902f153935f2b31
Mostafa Mohamed <99538301+MostafaMohamedgamil@users.noreply.github.com>
2025-12-07 18:01:09 +0200

    first commit

---
+++ src/app/app-module.ts
@@ -0,0 +1,22 @@
+import {
+  NgModule,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { Header } from './shared/header/header';
+
+@NgModule({
+  declarations: [App],
+  imports: [BrowserModule, AppRoutingModule, Header],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideClientHydration(withEventReplay()),
+  ],
+  bootstrap: [App],
+})
+export class AppModule {}
